c7814472afcfdd3f59177a1e71d5577c3be7ce49
Jhonatan _Salgueiro <jhonatansalgueiro2002@gmail.com>
2026-07-03 17:11:33 -0300

    fix: import permission_classes decorator

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, status, filters
-from rest_framework.decorators import api_view, parser_classes, action
+from rest_framework.decorators import api_view, parser_classes, action, permission_classes
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import AllowAny, IsAuthenticated
